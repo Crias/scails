02b7f300fb37016a8016011ffee0c5290ac498bb
Travis Calder <travis.calder@gmail.com>
2011-07-12 23:21:12 -0600

    Initial version of the Lift 2.4-M1 initialization templates.

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ name := "Scails"
 organization := "com.codequirks"
 
 version := "0.1.0"
+
+libraryDependencies += "org.apache.velocity" % "velocity" % "1.7"
--- src/main/scala/scails/ScailsCommand.scala
+++ src/main/scala/scails/ScailsCommand.scala
@@ -1,34 +1,67 @@
 import sbt._
 import Keys._
+import java.io.StringWriter;
 import java.io.File
 import java.io.FileWriter
+import java.util.Properties
+import scala.collection.JavaConversions._
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity
+
 object ScailsCommand extends Plugin {
 
   val mvcImpl = new LiftApp
 
-  val liftVersion = SettingKey[String]("liftVersion")
-
-  override lazy val settings = Seq(commands += scailsMain)
+  override lazy val settings = Seq(
+    commands += scailsMain
+  )
   lazy val scailsMain = 
-    Command.single("scail") { (state: State, projectName : String) =>
-      println("About to scale " + projectName + "!")
+    Command.single("scail") { (state: State, appName : String) =>
+      println("About to scale " + appName + "!")
       state
-     scailApplication(state, projectName)
+     scailApplication(state, appName)
     }
 
-  def scailApplication(state : State, projectName : String) = {
+  def scailApplication(state : State, appName : String) = {
+    Velocity.setProperty("resource.loader", "class")
+    Velocity.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader")
+    Velocity.init()
+    val context = new VelocityContext()
+    context.put( "appName", appName)
+
     if(file("build.sbt").exists()) file("build.sbt").delete()
 
     mvcImpl.prepDirectories()
     writePlugins()
-    writeProject(projectName)
+    writeProject(appName)
 
-    IO.unzipStream(resourceStream("lift_2.4-M1.zip"), file("src/main"))
+    val initFolder = "init/lift_2.4-M1/"
+    readProps(initFolder+"template.properties").foreach { entry => 
+      val (key,value) = (entry.getKey(), entry.getValue()) 
+      if(value == null || "" == value.toString)
+        IO.transfer(resourceStream(initFolder + key), file(key.toString))
+      else {
+        val sw = new StringWriter
+        val template = Velocity.getTemplate(initFolder + key)
+        template.merge(context, sw)
+        IO.write(file(value.toString), sw.toString)
+      }
+    }
+    readProps(initFolder+"directory.properties").foreach { entry => 
+      file(entry.getKey.toString).mkdirs
+    }
 
-    setupCommands(projectName).foldLeft(state){(state : State, command : String) =>
+    setupCommands(appName).foldLeft(state){(state : State, command : String) =>
       Command.process(command,state)
     }
   }
+
+  def readProps(name:String) = {
+    val templates = new Properties()
+    templates.load(resourceStream(name))
+    templates.entrySet
+  }
+
 
   def resourceStream(name : String) = this.getClass().getClassLoader.getResourceAsStream(name)
 
@@ -40,14 +73,14 @@ object ScailsCommand extends Plugin {
     fw.close
   }
 
-  def writeProject(projectName : String) {
+  def writeProject(appName : String) {
     file("build.sbt").delete()
     val fw = new FileWriter(file("build.sbt"))
-    fw.write(projectBuildFile(projectName))
+    fw.write(projectBuildFile(appName))
     fw.close
   }
 
-  def setupCommands(projectName:String)  : List[String] = "reload" :: "update" :: Nil
+  def setupCommands(appName:String)  : List[String] = "reload" :: "update" :: Nil
 
   def pluginBuildFile = """
     |resolvers += "Web plugin repo" at "http://siasia.github.com/maven2"
@@ -55,7 +88,7 @@ object ScailsCommand extends Plugin {
     |libraryDependencies <+= sbtVersion(v => "com.github.siasia" %% "xsbt-web-plugin" % ("0.1.0-"+v))
     |""".stripMargin
 
-  def projectBuildFile(projectName:String) = "name := \""+projectName+"\"\n" +
+  def projectBuildFile(appName:String) = "name := \""+appName+"\"\n" +
     "\n" +
     "seq(webSettings :_*)\n" + 
     "\n" +
@@ -64,25 +97,25 @@ object ScailsCommand extends Plugin {
 }
 
 trait MVC {
-	val deps : String
-	def prepDirectories() : Unit
+  val deps : String
+  def prepDirectories() : Unit
 }
 
 class LiftApp extends MVC {
+
+
   def prepDirectories() {
-  	file("src/main/scala").mkdirs
-  	file("src/main/resources").mkdirs
-  	file("src/main/webapp").mkdirs
-  	file("src/test/scala").mkdirs
+    file("src/main/scala").mkdirs
+    file("src/main/resources").mkdirs
+    file("src/main/webapp").mkdirs
+    file("src/test/scala").mkdirs
   }
 
   val deps : String = """
-	|liftVersion := "2.4-M1"
-	|
-	|libraryDependencies <+= liftVersion { lV => "net.liftweb" %% "lift-webkit" % lV % "compile->default" }
-	|
-	|libraryDependencies <+= liftVersion { lV => "net.liftweb" %% "lift-mapper" % lV % "compile->default" }
-	|
-	|libraryDependencies <+= liftVersion { lV => "net.liftweb" %% "lift-wizard" % lV % "compile->default" }
-	|""".stripMargin   
+  |libraryDependencies +=  "net.liftweb" %% "lift-webkit" % "2.4-M1" % "compile->default"
+  |
+  |libraryDependencies += "net.liftweb" %% "lift-mapper" % "2.4-M1" % "compile->default"
+  |
+  |libraryDependencies += "net.liftweb" %% "lift-wizard" % "2.4-M1" % "compile->default"
+  |""".stripMargin   
 }
